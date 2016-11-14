7017914380e9b630597f4a639e8883746204e19a
aishwarya <aish.96jain@gmail.com>
2016-11-15 03:15:03 +0530

    final commit ,eh?

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var pg = require ('pg');
+
+var con_string = 'tcp://Dell-pc:rainbow@localhost/chatdb';
+
+var pg_client = new pg.Client(con_string);
+pg_client.connect();
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
- res.sendFile(__dirname + '/index.html');
+ res.sendFile(__dirname + '/about.html');
 });
-
+app.get('/try_it', function(req, res) {
+ res.sendFile(__dirname + '/index.html');
+   // res.render('about', { });
+});
 var user=0;
 
 io.on('connection', function(socket){
@@ -27,6 +36,14 @@ socket.on('notifyUser', function(user){
   });*/
 socket.on('chat message', function(data){
 	console.log(data.msg1 + " encrypted to " + data.msg2 + ", gender = "+data.gender);
+  // pg.connect(pg_client, function(err, client) {
+  // if (err) return console.log(err);
+  // else
+    pg_client.query("INSERT INTO chat values($1,$2,$3,$4)",[data.user,data.gender,data.msg1,data.msg2] ,function(err,results){
+      console.log(results);
+    // });  
+  });
+  
     io.emit('chat message', {user:data.user,gender:data.gender,msg1:data.msg1});
   });
 
@@ -37,6 +54,7 @@ socket.on('chat message', function(data){
   
 
 });
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
